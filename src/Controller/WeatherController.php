378729211ab93250e349c2fc4c61715f432b962c
Aleksandar Zivanovic <acas_mail@yahoo.com>
2024-01-04 10:11:09 +0100

    44. Route parameter requirements in a Controller

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -10,8 +10,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController
 {
-    // #[Route('/weather/highlandersays/{threshold}', methods:['GET', 'POST'])]
-    public function highlanderSays(int $threshold): Response
+
+    #[Route('/weather/highlandersays/{guess}', methods:['GET', 'POST'], priority:1)]
+    public function highlanderSaysGuess($guess): Response
+    {
+        $forecast ="It's going to $guess!";
+
+        return $this->render('weather/highlander_says.html.twig',
+            [
+                'forecast' => $forecast,
+            ]
+        );
+    }
+
+    #[Route('/weather/highlandersays/{threshold}', methods:['GET', 'POST'], requirements: ['threshold' => '\d+'], priority:2)]
+    public function highlanderSays(int $threshold = 50): Response
     {
 
         $draw = rand(1, 100);

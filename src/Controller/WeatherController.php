83a2660bf2ce6e4bd28080d4cebf2bcfb12cbe01
Aleksandar Zivanovic <acas_mail@yahoo.com>
2024-01-04 08:51:41 +0100

    WeatherController

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WeatherController extends AbstractController
+{
+    #[Route('/weather/highlandersays')]
+    public function highlanderSays(): Response
+    {
+
+        $draw = rand(1, 100);
+        $forecast = $draw < 50 ? "It's going to rain!" : "It's going to be sunny";
+
+        return $this->render('weather/highlander_says.html.twig',
+            [
+                'forecast' => $forecast,
+            ]
+        );
+    }
+}

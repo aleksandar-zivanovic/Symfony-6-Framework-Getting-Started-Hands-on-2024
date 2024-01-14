80c6e93d3681952c78f66b3144819dc95a05490e
Aleksandar Zivanovic <acas_mail@yahoo.com>
2024-01-14 14:08:32 +0100

    48. Specifying Host

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -5,12 +5,22 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/weather')]
 class WeatherController extends AbstractController
 {
+
+    #[Route('/highlandersays/{threshold<\d+>?50}', host:'api.localhost')]
+    public function highlanderSaysApi(int $threshold): Response
+    {
+        $draw = rand(1, 100);
+        $forecast = $draw < $threshold ? "It's going to rain!" : "It's going to be sunny";
+        $json = ['forecast' => $forecast];
+        return new JsonResponse($json);
+    }
 
     #[Route('/highlandersays/{threshold<\d+>?50}')]
     public function highlanderSays(int $threshold): Response

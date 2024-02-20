09d059b36dd7c7b4fed600b9543f2d4a91e8aaec
Aleksandar Zivanovic <acas_mail@yahoo.com>
2024-02-20 19:13:48 +0100

    107. Built-in Form Types

--- src/Controller/LocationFormController.php
+++ src/Controller/LocationFormController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Location;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,13 +17,37 @@ class LocationFormController extends AbstractController
     public function new(): Response
     {
         $location = new Location();
-        $location->setCountryCode('PL');
 
         $form = $this->createFormBuilder($location)
             ->add('name', TypeTextType::class)
-            ->add('countryCode')
-            ->add('latitude')
-            ->add('longitude')
+            ->add('countryCode', ChoiceType::class, [
+                'choices' => [
+                    '' => null,
+                    'Poland' => 'PL',
+                    'France' => 'FR',
+                    'Germany' => 'DE',
+                    'Spain' => 'ES',
+                    'United Kingdom' => 'UK',
+                    'United States' => 'US',
+                    'India' => 'IN',
+                ]
+            ])
+            ->add('latitude', NumberType::class, [
+                'html5' => true,
+                'attr' => [
+                    'min' => -90,
+                    'max' => 90,
+                    'step' => 0.1,
+                ]
+            ])
+            ->add('longitude', NumberType::class, [
+                'html5' => true,
+                'attr' => [
+                    'min' => 180,
+                    'max' => -180,
+                    'step' => 0.1,
+                ]
+            ])
             ->getForm();
 
 
